8e4996e63f0d0fd7abf3e74c66c09c0eec8991ab
valerybuko <valpal314@gmail.com>
2020-04-22 19:51:26 +0300

    add theme changing toggle

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const counter = document.getElementById('counter');
 const addBtn = document.getElementById('add');
 const subBtn = document.getElementById('sub');
 const asyncBtn = document.getElementById('async');
+const themeBtn = document.getElementById('theme');
 
 let state = 0;
 
@@ -15,15 +16,21 @@ addBtn.addEventListener('click', () => {
     state++;
     render()
 });
+
 subBtn.addEventListener('click', () => {
     state--;
     render()
 });
+
 asyncBtn.addEventListener('click', () => {
    setTimeout(() => {
        state++;
        render();
    }, 2000);
+});
+
+themeBtn.addEventListener('click', () => {
+   document.body.classList.toggle('dark');
 });
 
 render();
