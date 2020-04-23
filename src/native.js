6223328f52807100520daea88d9afd4847305a1e
valerybuko <valpal314@gmail.com>
2020-04-23 11:23:20 +0300

    create own createStore function

--- src/native.js
+++ src/native.js
@@ -1,5 +1,4 @@
-import { createStore } from "./createStore";
-import './styles.css';
+import './styles.css'
 
 const counter = document.getElementById('counter');
 const addBtn = document.getElementById('add');
@@ -7,22 +6,31 @@ const subBtn = document.getElementById('sub');
 const asyncBtn = document.getElementById('async');
 const themeBtn = document.getElementById('theme');
 
-let store = createStore();
+let state = 0;
+
+function render() {
+    counter.textContent = state.toString();
+}
 
 addBtn.addEventListener('click', () => {
-
+    state++;
+    render()
 });
 
 subBtn.addEventListener('click', () => {
-
+    state--;
+    render()
 });
 
 asyncBtn.addEventListener('click', () => {
-
+    setTimeout(() => {
+        state++;
+        render();
+    }, 2000);
 });
 
 themeBtn.addEventListener('click', () => {
-   // document.body.classList.toggle('dark');
+    document.body.classList.toggle('dark');
 });
 
-
+render();
